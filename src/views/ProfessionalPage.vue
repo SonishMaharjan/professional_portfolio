e530c54e4da6563a7a348067ac4afc6575d2fa4a
sonish <sonishmzn95@gmail.com>
2020-11-12 17:33:35 +0545

    navbar added

---
+++ src/views/ProfessionalPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="professional-page-wrapper">
+    <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/professional-page/Navbar";
+export default {
+  name: "ProfessionalPage",
+  components: { Navbar }
+};
+</script>
+
+<style>
+</style>
