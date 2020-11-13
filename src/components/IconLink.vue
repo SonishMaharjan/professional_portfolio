b57c0ecd80f408e84ae98faffd5d2bdc215315b0
sonish <sonishmzn95@gmail.com>
2020-11-13 16:09:55 +0545

    Icon link addedin nav bar

---
+++ src/components/IconLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a :href="href" :target="targetBlank ? '_blank' : false">
+    <slot> </slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "IconLink",
+  props: {
+    href: {
+      type: String,
+      default: "#"
+    },
+    targetBlank: {
+      type: Boolean
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/components/professional-page/Navbar.vue
+++ src/components/professional-page/Navbar.vue
@@ -8,7 +8,7 @@
         <a>Menu 2</a>
       </li>
     </ul>-->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
       <div class="container d-flex flex-row-reverse">
         <button
           class="navbar-toggler pull-right"
@@ -31,12 +31,33 @@
             <NavItem label="Contact"></NavItem>
           </ul>
 
-          <div class="my-2 d-flex flex my-lg-0">
-            <span>link</span>
-            <span>git</span>
+          <div
+            class="my-2 d-flex flex my-lg-0 justify-content-center icons-wrapper"
+          >
+            <IconLink
+              href="https://www.linkedin.com/in/sonish-maharjan/"
+              targetBlank
+            >
+              <font-awesome-icon
+                :icon="['fab', 'linkedin']"
+                class="icon linkedin-icon"
+              />
+            </IconLink>
+
+            <IconLink href="https://github.com/SonishMaharjan/" targetBlank>
+              <font-awesome-icon
+                :icon="['fab', 'github']"
+                class="icon github-icon"
+              />
+            </IconLink>
           </div>
 
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Resume</button>
+          <button
+            class="btn btn-outline-light my-2 my-sm-0 resume-button"
+            type="submit"
+          >
+            Resume
+          </button>
         </div>
       </div>
     </nav>
@@ -45,9 +66,17 @@
 
 <script>
 import NavItem from "@/components/NavItem";
+import IconLink from "@/components/IconLink";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faLinkedin, faGithub);
 export default {
   name: "Navbar",
-  components: { NavItem }
+  components: { NavItem, IconLink, FontAwesomeIcon }
 };
 </script>
 
@@ -55,5 +84,46 @@ export default {
 span {
   color: white;
   display: block;
+}
+
+.icons-wrapper {
+  padding-top: 0.3rem;
+  // border: 3px solid yellow;
+
+  .icon {
+    margin: 0.5rem;
+
+    &.github-icon {
+      color: #fff;
+      height: 25px;
+      width: 25px;
+
+      &:hover {
+        color: #ccc;
+      }
+    }
+
+    &.linkedin-icon {
+      color: #007bb5;
+      height: 25px;
+      width: 25px;
+
+      &:hover {
+        color: #06a;
+      }
+    }
+  }
+}
+
+.resume-button {
+  color: black;
+  font-family: "Montserrat", sans-serif;
+  // font-size: 2rem;
+  font-weight: 1000 !important;
+  margin: 0 0.5rem;
+
+  &:hover {
+    opacity: 0.9;
+  }
 }
 </style>
